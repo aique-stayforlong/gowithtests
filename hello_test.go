804dd3a328da0008bf7357cdab539fa9d40d7799
aique-stayforlong <aique.suarez@stayforlong.com>
2025-04-23 13:55:50 +0200

    add name and default value to hello function

--- hello_test.go
+++ hello_test.go
@@ -10,3 +10,26 @@ func TestHello(t *testing.T) {
 		t.Errorf("got %q, want %q", got, want)
 	}
 }
+
+func TestHelloDefault(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Aique")
+		want := "Hello, Aique"
+
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() // permite hacer referencia a la línea del test si algo falla
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
